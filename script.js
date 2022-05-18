340d4ac230e765203870eb0cd573ebc26dbc3321
Matteo Ciancone <matteo.ciancone@gmail.com>
2022-05-18 13:46:55 +0200

    wip

--- script.js
+++ script.js
@@ -2,29 +2,31 @@ let cells = document.querySelectorAll(".cell");
 cells = Array.from(cells);
 const playerTurn = document.querySelector(".playerTurn");
 const repeatBtn = document.querySelector(".repeatBtn");
+let winner = document.querySelector(".winner");
 
-let currentPlayer = "x";
+let currentPlayer = "X";
 playerTurn.innerHTML = "X's turn"
 
 const winComb = [
     [0, 1, 2],
     [2, 4, 5],
     [6, 7, 8],
-    [0.3, 6],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
 ]
 
+
 cells.forEach(cell => {
     cell.addEventListener("click", function () {
         cell.innerHTML = currentPlayer;
-        if (currentPlayer == "x") {
-            currentPlayer = "o";
+        if (currentPlayer == "X") {
+            currentPlayer = "O";
             playerTurn.innerHTML = "O's turn"
-        } else if (currentPlayer = "o") {
-            currentPlayer = "x";
+        } else if (currentPlayer = "O") {
+            currentPlayer = "X";
             playerTurn.innerHTML = "X's turn"
         }
         checkWinner()
@@ -33,8 +35,15 @@ cells.forEach(cell => {
 
 function checkWinner() {
     winComb.forEach(comb => {
-        comb.
+        let check = comb.every(i => cells[i].innerHTML.trim() == currentPlayer)
+        if (check) {
+            winner.innerHTML = `${currentPlayer} has win the game!`
+        }
     })
 }
 
+function refreshPage() {
+    window.location.reload()
+}
 
+repeatBtn.addEventListener("click", refreshPage)
